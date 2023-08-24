842296eefdceb8ed0d2495379fc43eeefc6f772a
Lautaro Sanchez Chandia <lautarosanche@gmail.com>
2023-08-24 05:07:34 -0300

    DespliegueNodeFront

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import  express  from "express";
+import { dirname,join} from "path";
+import {fileURLToPath  } from "url";
+import  indexRoutes  from './routes/index.js'
+const app=express()
+const __dirname=dirname(fileURLToPath(import.meta.url))
+console.log(__dirname,'views')
+app.set('views',join(__dirname,'views'))
+app.set('view engine','ejs')
+app.use(indexRoutes)
+app.use(express.static(join(__dirname,'public')))
+app.listen(3000)
+console.log("SERVER ESCUCHANDO EN EL PUERTO",3000)
